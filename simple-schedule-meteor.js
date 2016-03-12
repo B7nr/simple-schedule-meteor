2e5c220607c4bf724478e12b1e60877998a73637
B7nr <ben@rehf27.com>
2016-03-12 13:52:59 -0600

    Added Slots collection

--- simple-schedule-meteor.js
+++ simple-schedule-meteor.js
@@ -1,10 +1,10 @@
+Tasks = new Mongo.Collection("slots");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    slots: [
-      { text: "This is slot 1" },
-      { text: "This is slot 2" },
-      { text: "This is slot 3" }
-    ]
+    slots: function () {
+      return Tasks.find({});
+    }
   });
 }
