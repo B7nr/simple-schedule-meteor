bc476a06584c524134647480f84a82d08f49dfdf
B7nr <ben@rehf27.com>
2016-03-12 14:40:46 -0600

    Insert and update

--- simple-schedule-meteor.js
+++ simple-schedule-meteor.js
@@ -1,10 +1,40 @@
-Tasks = new Mongo.Collection("slots");
+Slots = new Mongo.Collection("slots");
 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     slots: function () {
-      return Tasks.find({});
+      return Slots.find({}, {sort: {createdAt: -1}});
     }
   });
+// Adding slots with a form
+  Template.body.events({
+    "submit .new-slot": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Slots.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+    Template.calender.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Slots.update(this._id, {
+                $set: {checked: ! this.checked}
+            });
+        },
+        "click .delete": function () {
+            Slots.remove(this._id);
+        }
+    });
 }
